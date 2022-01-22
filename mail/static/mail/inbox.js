7a0a39ebc91e350c93389a53049bdbc1df4ea749
Tej <tejpartap02@gmail.com>
2022-01-22 17:49:38 -0500

    Add functional to send emails using apis. Emails get saved in the database without change of url.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
@@ -11,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
-
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -20,6 +18,59 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  
+  
+  // Get values from of all the fields after submit is clicked()
+  document.querySelector("#compose-form").onsubmit = function(){
+    try {
+      document.querySelector('#message').remove();  
+    }
+    catch (exception_var) {
+      console.log("No message was found")
+    }
+    let recipients = document.querySelector("#compose-recipients").value;
+    let subject = document.querySelector("#compose-subject").value;
+    let body = document.querySelector("#compose-body").value;
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result=>{
+      const element = document.createElement('div');
+      element.setAttribute("id", "message")
+      if(result["message"] === "Email sent successfully."){
+          element.style.color = "green";
+          element.innerHTML = result["message"];
+      }
+      else{
+          element.style.color = "red";
+          element.innerHTML = result["error"];
+      }
+      document.querySelector("#compose-view").append(element);    
+
+
+      // TODO: Tej : This code needs improvement
+      // let myVar = setInterval(()=>{
+      //   let messages = document.querySelectorAll(".message")
+      //   for(let i = 0; i < messages.length; i++){
+      //     messages[i].remove();
+      //   }
+      //   clearInterval(myVar);
+            
+      //   }, 2000);
+        
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+    return false;
+  };
 }
 
 function load_mailbox(mailbox) {
